484c18a54db183f316033d3b121bdbf0a1d7a389
sktanui <shadrackkemei2019@gmail.com>
2024-09-23 14:24:46 +0300

    third complete lab

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(rideDistance) {
   // Write your code here!
+  if (rideDistance > 2500) {
+    return 'No can do.';
+  } else if (rideDistance > 2000) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (rideDistance > 400) {
+    return 'That will be twenty bucks.';
+  } else {
+    return 'This one is on me!';
+  }
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
   // Write your code here!
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:   
+
+      return 'Bye.';
+  }
 }
